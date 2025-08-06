65c7fdf070694bc94d8ba451ff469075b57d3e20
Account-not-found <nonchalantdreadheadass@gmail.com>
2025-08-06 22:27:56 +1200

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,18 +1,37 @@
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 
-for _, v in pairs(Players:GetPlayers()) do
-    if v ~= Players.LocalPlayer and not v.Character:FindFirstChild("Highlight") then
-        local hl = Instance.new("Highlight", v.Character)
-        hl.FillColor = Color3.new(1, 0, 0)
-        hl.OutlineColor = Color3.new(1, 1, 1)
+-- esp.lua
+if _G.ESPEnabled then
+    _G.ESPEnabled = false
+    for _, v in pairs(game.CoreGui:GetChildren()) do
+        if v.Name == "ESPHighlight" then v:Destroy() end
+    end
+    return
+end
+
+_G.ESPEnabled = true
+
+local function createESP(char)
+    local highlight = Instance.new("Highlight")
+    highlight.Name = "ESPHighlight"
+    highlight.FillColor = Color3.new(1, 0, 0)
+    highlight.OutlineColor = Color3.new(1, 1, 1)
+    highlight.FillTransparency = 0.5
+    highlight.OutlineTransparency = 0
+    highlight.Adornee = char
+    highlight.Parent = game.CoreGui
+end
+
+for _, p in pairs(game.Players:GetPlayers()) do
+    if p ~= game.Players.LocalPlayer and p.Character then
+        createESP(p.Character)
     end
 end
 
-Players.PlayerAdded:Connect(function(plr)
-    plr.CharacterAdded:Connect(function(char)
-        local hl = Instance.new("Highlight", char)
-        hl.FillColor = Color3.new(1, 0, 0)
-        hl.OutlineColor = Color3.new(1, 1, 1)
+game.Players.PlayerAdded:Connect(function(p)
+    p.CharacterAdded:Connect(function(c)
+        if _G.ESPEnabled then createESP(c) end
     end)
 end)
+
