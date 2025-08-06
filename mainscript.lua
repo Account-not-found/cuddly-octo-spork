9ccb913292ceffe731951a6e885ba086e19d234d
Account-not-found <nonchalantdreadheadass@gmail.com>
2025-08-06 20:20:33 +1200

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -1,2 +1,2 @@
 -- Main entry for modular script hub
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Account-not-found/cuddly-octo-spork/refs/heads/ui/modular_gui.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Account-not-found/cuddly-octo-spork/refs/heads/ui/key_system.lua"))()
