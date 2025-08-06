eb77564445604bca69340461ebb656ee01b159c7
Account-not-found <nonchalantdreadheadass@gmail.com>
2025-08-06 21:29:00 +1200

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,3 +1,18 @@
--- Simple ESP
--- Replace with advanced drawing ESP
-print("ESP module loaded")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+for _, v in pairs(Players:GetPlayers()) do
+    if v ~= Players.LocalPlayer and not v.Character:FindFirstChild("Highlight") then
+        local hl = Instance.new("Highlight", v.Character)
+        hl.FillColor = Color3.new(1, 0, 0)
+        hl.OutlineColor = Color3.new(1, 1, 1)
+    end
+end
+
+Players.PlayerAdded:Connect(function(plr)
+    plr.CharacterAdded:Connect(function(char)
+        local hl = Instance.new("Highlight", char)
+        hl.FillColor = Color3.new(1, 0, 0)
+        hl.OutlineColor = Color3.new(1, 1, 1)
+    end)
+end)
