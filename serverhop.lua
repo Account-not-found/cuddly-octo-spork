5d0d09c27d5f90c94868f46d34b2ddebdbfc392e
Account-not-found <nonchalantdreadheadass@gmail.com>
2025-08-06 22:33:45 +1200

    Update serverhop.lua

--- serverhop.lua
+++ serverhop.lua
@@ -1,27 +1,23 @@
+-- serverhop.lua
 local HttpService = game:GetService("HttpService")
-local TPS = game:GetService("TeleportService")
-local player = game.Players.LocalPlayer
-local servers = {}
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
 
-local req = syn and syn.request or http and http.request or http_request or request
-local function getServers()
-    local cursor = ""
-    while true do
-        local response = req({
-            Url = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100&cursor=" .. cursor
-        })
-        local body = HttpService:JSONDecode(response.Body)
-        for _, server in pairs(body.data) do
-            if server.playing < server.maxPlayers then
-                table.insert(servers, server.id)
-            end
+local function serverHop()
+    local servers = {}
+    local req = game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")
+    local body = HttpService:JSONDecode(req)
+    for _, server in pairs(body.data) do
+        if server.playing < server.maxPlayers and server.id ~= game.JobId then
+            table.insert(servers, server.id)
         end
-        if not body.nextPageCursor then break end
-        cursor = body.nextPageCursor
+    end
+    if #servers > 0 then
+        TeleportService:TeleportToPlaceInstance(game.PlaceId, servers[math.random(1, #servers)], player)
+    else
+        warn("⚠️ No available servers.")
     end
 end
 
-getServers()
-if #servers > 0 then
-    TPS:TeleportToPlaceInstance(game.PlaceId, servers[math.random(1, #servers)], player)
-end
+pcall(serverHop)
