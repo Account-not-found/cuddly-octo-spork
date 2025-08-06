759056e02379ee6824ef5da53c26e0db79f2da96
Account-not-found <nonchalantdreadheadass@gmail.com>
2025-08-06 21:25:50 +1200

    Update noclip.lua

--- noclip.lua
+++ noclip.lua
@@ -1,3 +1,15 @@
--- Simple noclip
--- Add noclip toggle here
-print("Noclip module loaded")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local player = Players.LocalPlayer
+
+local noclip = true
+
+RunService.Stepped:Connect(function()
+    if noclip and player.Character then
+        for _, v in pairs(player.Character:GetDescendants()) do
+            if v:IsA("BasePart") and v.CanCollide then
+                v.CanCollide = false
+            end
+        end
+    end
+end)
